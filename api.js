8394730db436f3ef3b2c4e6f20bbc2727969d25b
bycz6 <bycz@byczhome>
2017-07-12 01:50:03 +0100

    timout

--- api.js
+++ api.js
@@ -163,9 +163,8 @@ function ver_date_post(result) {
 
 }
 
-// verifica se o artigo foi criado à mais horas do que as passadas em
-// atributo
-function ver_date(time) {
+// cria a data de expiração do post de registo
+function ver_date(timeout) {
 	// return true;
 	steem.api.getContent(pAuthor, permlink,
 			function(err, result) {
@@ -175,23 +174,6 @@ function ver_date(time) {
 				registerdate = release_time.setHours(release_time.getHours()
 						+ timeout);
 				console.log("Register end date: " + new Date(registerdate));
-
-				// console.log("Bypass ver_date activated!");
-				// return true; // #### bypass ####
-
-				// if (new Date() > release_time.setHours(release_time
-				// .getHours()
-				// + timeout)) {
-				// console
-				// .log("Time ok! Passed - Could start registering");
-				// return true;
-				// } else {
-				// console
-				// .log("Do not pass the deadline! - Please try again in xpto
-				// horas!");
-				// return false;
-				//
-				// }
 
 			});
 }
