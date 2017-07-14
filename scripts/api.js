1774416a8b7c07cd42bafb12d08677fe4c84c109
bycz6 <bycz@byczhome>
2017-07-15 00:54:52 +0100

    init

--- scripts/api.js
+++ scripts/api.js
@@ -1,5 +1,10 @@
 /*
  * Here you can find ready to use scripts transversal for any project you may have
  * 
-*/
+ */
+var author;
 
+function init_regist() {
+	author = document.getElementById('name').value;
+	cosole.log("User name iput: @" + author);
+}
