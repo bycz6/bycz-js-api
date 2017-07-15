390c950c7813ba3f22acb2d79e5b720842d9a0eb
bycz6 <bycz@byczhome>
2017-07-15 02:30:03 +0100

    reading post votes

--- scripts/api.js
+++ scripts/api.js
@@ -5,6 +5,37 @@
 var author;
 
 function init_regist() {
-	author = document.getElementById('name').value;
-	console.log("User name input: @" + author);
+	parse_link(document.getElementById('link').value);
+
+	steem.api.getContent(author, permlink, function(err, result) {
+		console.log(err, result);
+		console.log("Init reading contente from @" +author " post-->" +permlink);
+		console.log("Number of upvotes: "+result.net_votes);
+		
+		
+	});
+}
+
+var openFile = function(event) {
+	var input = event.target;
+
+	var reader = new FileReader();
+	reader.onload = function() {
+		var text = reader.result;
+		var node = document.getElementById('output');
+		node.innerText = text;
+		console.log(reader.result.substring(0, 200));
+	};
+	reader.readAsText(input.files[0]);
+};
+
+// extrai do link do artigo o autor e o permlink
+// parse_link(document.getElementById('link').value); ---> put where to run it
+function parse_link(link) {
+	var at = link.indexOf("@");
+	var p = link.substring(at, at.length);
+
+	var slash = p.indexOf("/");
+	author = p.substring(1, slash);
+	var permlink = p.substring(slash + 1, p.length);
 }
