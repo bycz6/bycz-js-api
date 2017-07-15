547bb550a7812f3e259eed847c16088f591e9092
bycz6 <bycz@byczhome>
2017-07-15 03:29:41 +0100

    s

--- scripts/api.js
+++ scripts/api.js
@@ -39,18 +39,7 @@ function init_regist() {
 
 }
 
-var openFile = function(event) {
-	var input = event.target;
 
-	var reader = new FileReader();
-	reader.onload = function() {
-		var text = reader.result;
-		var node = document.getElementById('output');
-		node.innerText = text;
-		console.log(reader.result.substring(0, 200));
-	};
-	reader.readAsText(input.files[0]);
-};
 
 // extrai do link do artigo o autor e o permlink
 // parse_link(document.getElementById('link').value); ---> put where to run it
